57d7272ebb4d6aacf9d54de095ea6f02f01f08d1
Ahmad-Sajjad <144416634+Ahmad-Sajjad@users.noreply.github.com>
2023-09-21 17:32:38 +0500

    task11.cpp

--- task11.cpp
+++ task11.cpp
@@ -5,7 +5,7 @@ main()
     long int current,monthly,total;
     cout << "Enter the current world population: ";
     cin >> current;
-    cout << "Enter the monthly birth rate (number of births per month):";
+    cout << "Enter the monthly birth rate (number of births per month): ";
     cin>>monthly;
     total = (30 * 12 * monthly) + current;
     cout<<"The population in three decades will be: "<<total;
